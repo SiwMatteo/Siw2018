8dfab0c6b2cde09ec6cfc423007943cf41e47b75
Francesco <francesco.fo@live.it>
2018-06-21 14:23:23 +0200

    Modifiche finali

--- src/main/java/it/uniroma3/spring/security/SecurityConfig.java
+++ src/main/java/it/uniroma3/spring/security/SecurityConfig.java
@@ -1,5 +1,5 @@
 package it.uniroma3.spring.security;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,12 +13,9 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
- 
- 
-	
-	
-	 @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
-	    }
-    }
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+	}
+}
